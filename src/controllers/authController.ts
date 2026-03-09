31ed36fd0cdd0ec14e44163df2e437dc61a90c9d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-09 14:15:25 +1100

    feat(authController): if server-side errors found, return 400 error status and informative json
    json includes the last-inputted email and username inputs so that it can be restored in the ui if desired.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,17 @@
 import type { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
 import validateSignupForm from "@/middleware/validateSignupForm";
 
 const signupPost = [
 	validateSignupForm,
 	(req: Request, res: Response) => {
+		const formData = matchedData(req, { onlyValidData: false });
+		const { email, username } = formData;
+
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.status(400).json({ errors: errors.array(), email, username });
+
 		res.json("signup route hit");
 	},
 ];
