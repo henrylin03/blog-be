7c046c480a5eda84ca543815b74e45858417fced
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-10 16:01:16 +1100

    feat(authRouter): add fake protected route
    calls the passport.authenticate on jwt strategy to confirm working

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import passport from "passport";
 import { loginPost } from "@/controllers/authController";
 
 const authRouter = Router();
 
+authRouter.get("/protected", passport.authenticate("jwt", { session: false }));
 authRouter.post("/login", loginPost);
 
 export default authRouter;
