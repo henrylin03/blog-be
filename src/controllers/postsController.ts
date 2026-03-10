59cc5c5af8f120791b17ecb57f8487cd30d49ab9
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-10 20:08:18 +1100

    feat(posts): new post is validated using express-validator (server-side validation). fix type error for user object (attached by passportjs). create new post using prisma, then return it.

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -1,11 +1,35 @@
 import type { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
 import { prisma } from "@/lib/prisma";
 import {
 	authenticateWithJwt,
 	confirmUserIsAuthorised,
 } from "@/middleware/auth";
+import validatePost from "@/middleware/validation/validatePost";
+import type { AuthenticatedRequest } from "@/types/types";
 
-const addNewPost = [authenticateWithJwt, confirmUserIsAuthorised];
+const addNewPost = [
+	authenticateWithJwt,
+	confirmUserIsAuthorised,
+	validatePost,
+	async (req: AuthenticatedRequest, res: Response) => {
+		const data = matchedData(req, { onlyValidData: false });
+		const { title, text } = data;
+
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.status(400).json({ errors: errors.array(), title, text });
+
+		try {
+			const newPost = await prisma.post.create({
+				data: { title, text, authorId: String(req.user.id) },
+			});
+			res.status(201).json({ message: "New post created", newPost });
+		} catch (error) {
+			res.status(500).json({ error });
+		}
+	},
+];
 
 const getPublishedPosts = async (_req: Request, res: Response) => {
 	const publishedPosts = await prisma.post.findMany({
