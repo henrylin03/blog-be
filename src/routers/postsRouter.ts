78c8e6e60ad7e7fe53b4b279ec06fd07259339b9
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-11 14:06:28 +1100

    refactor(postsRouter): rename path for creating new post
    /new is unnecessary if we're already POSTing to /posts. BREAKING CHANGE.

--- src/routers/postsRouter.ts
+++ src/routers/postsRouter.ts
@@ -15,8 +15,7 @@ postsRouter.get("/:postId", getPost);
 postsRouter.put("/:postId", editPost);
 //? .patch for publishing?? or just have the req.body have it in form so when user clicks 'publish' (instead of 'save to draft') we also send 'isPublished = true'??
 
-postsRouter.post("/new", addNewDraftPost); // TODO: change to JUST '/'
-// TODO: admin users can also _publish_ post
+postsRouter.post("/", addNewDraftPost);
 
 // TODO: after auth set up, /my-posts will return all posts of that user (assuming they are an author) - published or not. this will be like a dashboard.
 postsRouter.use("/:postId/comments", commentsRouter);
